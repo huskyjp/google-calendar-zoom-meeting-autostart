4b8461e17148f474fe768468ceef18a505853b06
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-04-26 21:38:57 -0700

    first commit

---
+++ google/google_calendar.go
@@ -0,0 +1,140 @@
+package google
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+const (
+	calendarToken = "/google-calendar-token"
+	credentials   = "credentials.json"
+)
+
+type GoogleCalendarMeeting struct {
+	Title      string
+	StartTime  time.Time
+	MeetingURL string
+}
+
+func GetUpcomingMeeting() (GoogleCalendarMeeting, error) {
+	google := GoogleCalendarMeeting{}
+	client, err := newGoogleCalendarClient()
+	fmt.Println("err: ", err)
+	if err != nil {
+		return google, err
+	}
+	eventService := calendar.NewEventsService(client)
+	eventListner := eventService.List("primary")
+	eventListner.MaxResults(3)
+	eventListner.SingleEvents(true)
+	eventListner.OrderBy("starttime")
+
+	eventListner.TimeMin(time.Now().Format(time.RFC3339))
+	eventListner.TimeMax(time.Now().Add(time.Minute * 30).Format(time.RFC3339))
+
+	events, err := eventListner.Do()
+	if err != nil {
+		fmt.Println("error retrieving events: ", err)
+		return google, err
+	}
+	fmt.Println("current events inside: ", events)
+	if len(events.Items) == 0 {
+		fmt.Println("there is no meetings is scheduled at this moment")
+	}
+
+	fmt.Println("evnts: ", events)
+	eventOne := events.Items[0]
+	eventTwo := events.Items[1]
+	fmt.Println(eventOne.Summary)
+	fmt.Println(eventTwo.Summary)
+
+	// get meeting title
+	google.Title = eventOne.Summary
+
+	meetingTime, err := time.Parse(time.RFC3339, eventOne.Start.DateTime)
+	// get meeting start time
+	google.StartTime = meetingTime
+
+	if eventOne.ConferenceData == nil {
+		return google, nil
+	}
+
+	for _, entry := range eventOne.ConferenceData.EntryPoints {
+		if entry.EntryPointType == "video" {
+			fmt.Println(entry.Uri)
+			google.MeetingURL = entry.Uri
+			break
+
+		}
+
+	}
+
+	return google, err
+
+}
+
+// client for google calender to fetch its data
+func newGoogleCalendarClient() (*calendar.Service, error) {
+	newCredential, err := ioutil.ReadFile(credentials)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse json to *0auth2.Config type
+	newConfig, err := google.ConfigFromJSON(newCredential, calendar.CalendarReadonlyScope)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := getGoogleCalendarToken(newConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	// get access to the Calendar API
+	ctx := context.Background()
+	return calendar.NewService(ctx, option.WithTokenSource(newConfig.TokenSource(ctx, token)))
+
+}
+
+// cli handler
+func fetchNewToken(config *oauth2.Config) (*oauth2.Token, error) {
+	// get authURL: provide authentication page URL
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authentication code: %s\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		return &oauth2.Token{}, fmt.Errorf("Unable to read authorization ode: &v\n", err)
+	}
+
+	token, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		return &oauth2.Token{}, fmt.Errorf("unable to exchange authCode for token: %s", err)
+	}
+	// token is type of *oauth2.Token
+	return token, err
+}
+
+func getGoogleCalendarToken(config *oauth2.Config) (*oauth2.Token, error) {
+	token, err := GetCredidentialGoogleCalendarTokenFromFile()
+	if err != nil {
+		fmt.Printf("Error getting stashed token: %s \n", err)
+
+		token, err := fetchNewToken(config) // parse config and get new token
+		fmt.Println("current   ", token)
+		if err != nil {
+			return token, err
+		}
+		WriteGoogleCalendarToken(token) // only returns error but creates new file
+		return token, nil
+	}
+	return token, nil
+}
